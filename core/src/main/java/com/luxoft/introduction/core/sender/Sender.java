a416285777f9d0005e8039cf05ee5c2658c0f343
Prosner <phep45@gmail.com>
2016-03-15 16:17:50 +0100

    sender class partly completed

--- core/src/main/java/com/luxoft/introduction/core/sender/Sender.java
+++ core/src/main/java/com/luxoft/introduction/core/sender/Sender.java
@@ -1,5 +1,7 @@
 package com.luxoft.introduction.core.sender;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -20,8 +22,46 @@ public class Sender {
     private QueueSender queueSender;
     private TextMessage textMessage;
 
-    public Sender(String queueName) {
-        this.queueName = queueName;
+    public Sender(String queueName) throws JMSException {
+
+        // Create a ConnectionFactory
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("http://localhost");
+
+        // Create a Connection
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        // Create a Session
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // Create the destination (Topic or Queue)
+        Destination destination = session.createQueue(queueName);
+
+        // Create a MessageProducer from the Session to the Topic or Queue
+        MessageProducer producer = session.createProducer(destination);
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+        // Create a messages
+        String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
+        TextMessage message = session.createTextMessage(text);
+
+        // Tell the producer to send the message
+        System.out.println("Sent message: "+ message.hashCode() + " : " + Thread.currentThread().getName());
+        producer.send(message);
+
+        // Clean up
+        session.close();
+        connection.close();
+
+
+//        MQConnectionFactory cf = new MQConnectionFactory();
+//        cf.setHostName(HOSTNAME);
+//        cf.setPort(PORT);
+//        cf.setChannel(CHANNEL);
+//        cf.setQueueManager(QMNAME);
+//        cf.setTransportType(WMQConstants.WMQ_CM_CLIENT);
+
+        /*this.queueName = queueName;
         try {
             jndiContext = new InitialContext();
             queueConnectionFactory = (QueueConnectionFactory) jndiContext.lookup("QueueConnectionFactory");
@@ -34,7 +74,7 @@ public class Sender {
             e.printStackTrace();
         } catch (JMSException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     public void sendMessage(String message) {
@@ -50,7 +90,7 @@ public class Sender {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JMSException {
         Sender sender = new Sender("myQueue");
         for(int i = 0; i < 5; i++)
             sender.sendMessage("test message #" + i);
