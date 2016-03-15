b92aeeb7d846501e31655b0472030c01c9d0d9d0
Prosner <phep45@gmail.com>
2016-03-15 11:26:54 +0100

    Sender class added

---
+++ core/src/main/java/com/luxoft/introduction/core/sender/Sender.java
@@ -0,0 +1,12 @@
+package com.luxoft.introduction.core.sender;
+
+/**
+ * Created by Prosner on 3/15/2016.
+ *
+ */
+public class Sender {
+
+    public static void main(String[] args) {
+        System.out.println("JMS Sender");
+    }
+}
